9efdf36df9b653196f6e375f4fdd6f064cc941b2
taimoorchoudhary <taimoor@aurorasolutions.io>
2015-02-19 17:24:10 +0500

    (TH-173) Added 'View Model', 'Model' and 'ServiceStatusToBrushConverter.cs' for Service Configuration. Modified WPF to populate service details dynamically.

---
+++ TradeHubGui.Common/Constants/ServiceStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeHubGui.Common.Constants
+{
+    /// <summary>
+    /// Contains possible status's for the Application Services
+    /// </summary>
+    public enum ServiceStatus
+    {
+        Disabled,
+        Starting,
+        Running,
+        Stopping,
+        Stopped
+    }
+}
---
+++ TradeHubGui/Converters/ServiceStatusToBrushConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using TradeHubGui.Common.Constants;
+
+namespace TradeHubGui.Converters
+{
+    public class ServiceStatusToBrushConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts order status to SolidColorBrush
+        /// </summary>
+        /// <param name="value">ServiceStatus</param>
+        /// <param name="targetType">not used</param>
+        /// <param name="parameter">not used</param>
+        /// <param name="culture">not used</param>
+        /// <returns>System.Windows.Media.SolidColorBrush</returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null && value != DependencyProperty.UnsetValue)
+            {
+                var status = (ServiceStatus)value;
+
+                // Return certain brush depending on OrderStatus
+                if (status.Equals(ServiceStatus.Disabled))
+                {
+                    return Application.Current.Resources["GrayBrush"];
+                }
+                else if (status.Equals(ServiceStatus.Starting))
+                {
+                    return Application.Current.Resources["MediumTurquoiseBrush"];
+                }
+                else if (status.Equals(ServiceStatus.Running))
+                {
+                    return Application.Current.Resources["GreenBrush"];
+                }
+                else if (status.Equals(ServiceStatus.Stopping))
+                {
+                    return Application.Current.Resources["OrangeBrush"];
+                }
+                else if (status.Equals(ServiceStatus.Stopped))
+                {
+                    return Application.Current.Resources["RedBrush"];
+                }
+            }
+
+            // default color is Gray
+            return Application.Current.Resources["GrayBrush"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
