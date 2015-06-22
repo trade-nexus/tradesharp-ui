6bd31a7f2345481d9b16c61a1dc4b7ed1278813d
taimoorchoudhary <taimoor@aurorasolutions.io>
2015-06-22 12:21:52 +0500

    Updated assembly name to 'TradeSharp' and added icon to .exe file

--- TradeHubGui/Properties/AssemblyInfo.cs
+++ TradeHubGui/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TradeHUB")]
+[assembly: AssemblyTitle("TradeSharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TradeHUB")]
+[assembly: AssemblyProduct("TradeSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
